1a7b70b2667d2e372ad7511beb93770306b753d2
Fabien Boscher <fabien.boscher@epitech.digital>
2025-09-02 13:21:48 +0200

    parti2

---
+++ partie2.py
@@ -0,0 +1,9 @@
+def compare (a,b) :
+    if (a>b) :
+        print("Le premier nombre est plus grand que le second")
+    if (a<b) :
+        print("Le premier nombre est plus petit que le second")
+    if (a==b) :
+        print('Les deux nombres sont égaux')
+
+compare (5,6)
